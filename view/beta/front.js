f96e069cf0c069d5ad2097054e6f5cec40e17228
andrewh-code <andrewh.code@gmail.com>
2016-02-17 16:17:01 -0500

    trying to add chart to main.html
    trying to add highchart to main.html

--- view/beta/front.js
+++ view/beta/front.js
@@ -30,6 +30,7 @@ function jsFunction(){
     
     document.getElementById("output").innerHTML = txt_input;
     document.getElementById("outputTechnicalIndicator").innerHTML = cb_200day;
+    document.getElementByID("js_chart").innerHTML = test();
 }
 
 function checkTechnicalIndicators(input, indicator){
@@ -44,3 +45,43 @@ function checkTechnicalIndicators(input, indicator){
 }
 
 //http://codereview.stackexchange.com/questions/24172/get-the-value-of-the-name-attribute-of-each-element-with-class-class
+
+function test () {
+    
+    var files = new Array('bbry.json', 'apple_stock.json');
+    //$.getJSON('msft.json', function (data) {
+    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {  
+        console.log(data);
+        // Create the chart
+        $('#container').highcharts('StockChart', {
+
+
+            rangeSelector : {
+                selected : 1
+            },
+
+            title : {
+                text : 'BBRY Stock Prices'
+            },
+
+            series : [{
+                name : 'BBRY',
+                data : data,
+                tooltip: {
+                    valueDecimals: 2
+                }
+            }]
+        });
+    });
+    
+    
+   $('#container').highcharts().show();
+   
+    
+};
+
+//http://jsfiddle.net/lorwynz_11/sSLnn/3/
+//http://jsfiddle.net/NxEnH/89/
+//http://www.java2s.com/Tutorials/highcharts/Example/Series_Event/Hide_series_in_click_event.htm
+//http://jsfiddle.net/gh/get/jquery/1.9.1/highslide-software/highcharts.com/tree/master/samples/stock/members/chart-addseries/
+
